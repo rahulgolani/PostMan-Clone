ba0c4df39cf50301ec691c29e76ebdf0b9f5e95e
Rahul Golani <rahulgolani24@live.com>
2020-07-26 17:47:19 +0530

    visibility of Parameter box and Json Box

--- js/main.js
+++ js/main.js
@@ -1,1 +1,23 @@
 console.log("postman clone");
+
+//since the default value is json, so onload of the page the parameterbox should not display
+let parameterBox = document.getElementById('parameterBox');
+parameterBox.style.display = 'none';
+
+//getting the parameter radio
+let paramsRadio = document.getElementById('params');
+
+//adding event listener to manage visibility of parameter box and json box
+paramsRadio.addEventListener('click', () => {
+  document.getElementById('jsonBox').style.display = 'none';
+  document.getElementById('parameterBox').style.display = 'block';
+})
+
+//getting the json radio
+let jsonRadio = document.getElementById('json')
+
+//adding event listener to manage visibility of parameter box and json box
+jsonRadio.addEventListener('click', () => {
+  document.getElementById('parameterBox').style.display = 'none';
+  document.getElementById('jsonBox').style.display = 'block';
+})
